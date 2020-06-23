620939af692dd97d37487940bc73212faba0f4b4
Ayush Kadakia <ayush.kadakia@live.com>
2020-06-23 16:57:30 -0400

    Added getTax

--- tax.py
+++ tax.py
@@ -3,10 +3,16 @@ import unittest
 def addNums(num1, num2):
     return num1 + num2;
 
+def getTax(num1):
+    return round(.07 * num1, 2);
+
 class TestNumMethod(unittest.TestCase):
     
     def test_addNums(self):
         self.assertEqual(7, addNums(3, 4));
+
+    def test_getTax(self):
+        self.assertEqual(.70, getTax(10));
 
 if __name__ == "__main__":
     unittest.main()
